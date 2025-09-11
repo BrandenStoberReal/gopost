736dd37886b5bf9e1853d28b943a8b67be98c487
amnesia <amnesia@example.com>
2025-09-11 14:39:27 -0400

    Feat: Headers file

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func logThread(num int, msg string) {
 	log.Printf("[THREAD %d]: %s\n", num, msg)
 }
 
-func sendPostDataThreaded(url string, payload string, threadnum int, verbose bool, aggressive bool, origin string, referer string, useragent string) {
+func sendPostDataThreaded(url string, payload string, threadnum int, verbose bool, aggressive bool, headerLines []string) {
 	defer wg.Done()
 	if verbose && !aggressive {
 		logThread(threadnum, "New thread created and starting work.")
@@ -50,23 +50,10 @@ func sendPostDataThreaded(url string, payload string, threadnum int, verbose boo
 		check(err)
 
 		// Set up headers
-		req.Header.Set("Content-Type", "application/json")
-		if origin != "" {
-			req.Header.Set("origin", origin)
+		for _, header := range headerLines {
+			keyValue := strings.Split(header, "=")
+			req.Header.Set(keyValue[0], keyValue[1])
 		}
-		req.Header.Set("sec-ch-ua-platform", "\"Windows\"")
-		req.Header.Set("user-agent", useragent)
-		req.Header.Set("sec-ch-ua", "\"Not)A;Brand\";v=\"8\", \"Chromium\";v=\"138\", \"Google Chrome\";v=\"138\"")
-		req.Header.Set("sec-ch-ua-mobile", "?0")
-		req.Header.Set("accept", "*/*")
-		req.Header.Set("sec-fetch-site", "cross-site")
-		req.Header.Set("sec-fetch-mode", "cors")
-		req.Header.Set("sec-fetch-dest", "empty")
-		if referer != "" {
-			req.Header.Set("referer", referer)
-		}
-		req.Header.Set("accept-language", "en-US,en;q=0.9")
-		req.Header.Set("priority", "u=1, i")
 
 		// Set up proxy stuff
 		var proxy = proxiesList[helpers.RandomRange(0, len(proxiesList)-1)]
@@ -84,6 +71,7 @@ func sendPostDataThreaded(url string, payload string, threadnum int, verbose boo
 			continue // Normally we would check this, but this usually happens due to bad proxies that are user error. We will just cycle them instead.
 		}
 
+		// Log results
 		if !aggressive {
 			if verbose {
 				body, _ := io.ReadAll(resp.Body)
@@ -103,12 +91,12 @@ func sendPostDataThreaded(url string, payload string, threadnum int, verbose boo
 func main() {
 	urlPtr := flag.String("url", "", "URL to flood with POST requests. This should be the full URL to your endpoint.")
 	payloadPtr := flag.String("payload", "", "Path to a payload JSON file to send via POST flooding.")
-	originPtr := flag.String("origin", "", "URL to populate the origin header with.")
-	refererPtr := flag.String("referer", "", "URL to populate the referer header with.")
-	uaPtr := flag.String("ua", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36", "User-Agent to use for POST flooding.")
 	threadsPtr := flag.Int("threads", 16, "Number of goroutines to use.")
+	headersPtr := flag.String("headers", "", "Path to a txt file of headers to use. Format should be HEADER_NAME=HEADER_VALUE with newlines. Quotes are not added automatically.")
 	verbosePtr := flag.Bool("v", false, "Verbose output.")
 	speedModePtr := flag.Bool("a", false, "Do not log anything to console, aka maximum flood mode.")
+
+	headerLines := make([]string, 0) // Hehe slice
 
 	flag.Parse()
 
@@ -120,30 +108,31 @@ func main() {
 		log.Fatal("[ERROR] Payload is required.")
 	}
 
-	absPath, err := filepath.Abs(*payloadPtr)
+	// Payload
+	absPayloadPath, err := filepath.Abs(*payloadPtr)
 	check(err)
 
-	readPayload, err := os.ReadFile(absPath)
+	readPayload, err := os.ReadFile(absPayloadPath)
 	check(err)
+
+	// Headers
+	if *headersPtr != "" {
+		headerLines = strings.Split(*headersPtr, "\n")
+	}
 
 	log.Println("GoPOST v1.0.1 starting...")
 	log.Printf("URL: %s\n", *urlPtr)
 	log.Printf("Payload: %s\n", *payloadPtr)
-	if *originPtr != "" {
-		log.Printf("Origin: %s\n", *originPtr)
-	}
-	if *refererPtr != "" {
-		log.Printf("Referer: %s\n", *refererPtr)
-	}
 	log.Printf("Threads: %d\n", *threadsPtr)
 	log.Printf("Verbose: %t\n", *verbosePtr)
 	log.Printf("Aggressive: %t\n", *speedModePtr)
 
+	// Proxies
 	proxiesList = strings.Split(proxies, "\n")
 
 	for i := 0; i < *threadsPtr; i++ {
 		wg.Add(1)
-		go sendPostDataThreaded(*urlPtr, string(readPayload), i+1, *verbosePtr, *speedModePtr, *originPtr, *refererPtr, *uaPtr)
+		go sendPostDataThreaded(*urlPtr, string(readPayload), i+1, *verbosePtr, *speedModePtr, headerLines)
 	}
 	wg.Wait()
 }
